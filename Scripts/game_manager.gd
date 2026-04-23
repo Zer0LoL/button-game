efd60e906ac6561cfb03ae535cc8909060766234
Alvaro Ivan Roldan Candiotti <aroldan750@gmail.com>
2026-04-23 02:57:53 -0500

    Main title change , skins and new animation added

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var game_world = $GameWorld
 @onready var main_camera = $MainCamera
-@onready var title_label = $UI_Layer/Title
+@onready var animated_title = $UI_Layer/AnimatedTitle
 @onready var the_button = $GameWorld/TheButton 
 @onready var hud = $UI_Layer/UiInterface
 
@@ -16,13 +16,42 @@ var spawn_delay_time = 1.0
 var cooldown_time = 1.0    
 
 func _ready():
-	pass
+	setup_title_animations()
+	
+func setup_title_animations():
+	var letter_k = animated_title.get_child(0)
+	var letter_r = animated_title.get_child(1)
+	var gear_o = animated_title.get_child(2)
+	var letter_w = animated_title.get_child(3)
+	var letter_inc = animated_title.get_child(4)
+	
+
+	_force_skin(letter_k, "K")
+	_force_skin(letter_r, "R")
+	_force_skin(letter_w, "W")
+	_force_skin(letter_inc, "inc")
+	
+	var normal_letters = [letter_k, letter_r, letter_w, letter_inc]
+	for letter in normal_letters:
+		var state = letter.get_animation_state()
+		state.set_animation("Appear", false, 0)
+		state.add_animation("IDLE", 0.0, true, 0) 
+		
+	var gear_state = gear_o.get_animation_state()
+	gear_state.set_animation("IDLE Body", true, 0) 
+	gear_state.set_animation("IDLE Eye", true, 1)  
+
+func _force_skin(spine_node, skin_name):
+	var skeleton = spine_node.get_skeleton()
+	var skin_object = skeleton.get_data().find_skin(skin_name)
+	if skin_object:
+		skeleton.set_skin(skin_object)
 
 func button_clicked():
 	if workers_count == 0 and not is_prologue_active:
 	   
 		is_prologue_active = true
-		title_label.hide()
+		animated_title.hide()
 		
 		await get_tree().create_timer(2.0).timeout
 		spawn_worker()
--- Scripts/worker.gd
+++ Scripts/worker.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var spine_sprite = $WorkerVisual
 @onready var timer = Timer.new()
+@onready var cigar_smoke = $WorkerVisual/SpineBoneNode/CigarSmoke
 
 var is_dragging = false
 var drag_speed = 15.0 
@@ -14,20 +15,27 @@ func _ready():
 	add_child(timer)
 	timer.one_shot = true
 	timer.timeout.connect(_on_timer_timeout)
+	randomize_skin()
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and can_be_dragged:
-		is_dragging = event.pressed
-		if is_dragging:
-			
+		if event.pressed: # Clic presionado
+			is_dragging = true
 			is_moving = false
 			timer.stop()
-			spine_sprite.get_animation_state().set_animation("Run/Run", true, 0)
-		elif not is_dragging:
 			
-			spine_sprite.get_animation_state().set_animation("IDLE", true, 0)
-			_start_idle()
+			spine_sprite.get_animation_state().set_animation("Drag", true, 0)
 
+
+func _input(event):
+	if is_dragging and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+		is_dragging = false
+		
+		
+		target_position = global_position 
+		
+		spine_sprite.get_animation_state().set_animation("IDLE", true, 0)
+		_start_idle()
 func _process(delta):
 	if is_dragging:
 		var mouse_pos = get_global_mouse_position()
@@ -49,6 +57,18 @@ func _process(delta):
 	else:
 		spine_sprite.rotation = lerp(spine_sprite.rotation, 0.0, 5 * delta)
 
+func randomize_skin():
+	var available_skins = ["Bald", "Worker", "Boss"] 
+	var chosen_skin = available_skins.pick_random()
+	
+	var skeleton = spine_sprite.get_skeleton()
+	var skin_object = skeleton.get_data().find_skin(chosen_skin)
+	skeleton.set_skin(skin_object)
+	
+	if chosen_skin == "Boss":
+		cigar_smoke.emitting = true
+	else:
+		cigar_smoke.emitting = false
 
 func enable_interaction():
 	can_be_dragged = true
